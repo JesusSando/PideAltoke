52ff9c350f5ed259e39add2d0a90bbc97492eb23
FabianOsses21 <169071886+FabianOsses21@users.noreply.github.com>
2025-10-19 13:59:36 -0300

    Se agrego Pago y boleta
    Se creo pago y boleta

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import './assets/css/carrito.css'
-import '../src/assets/css/admin.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './assets/css/carrito.css';
+import '../src/assets/css/admin.css';
 
 import { Encabezado, Carrusel } from './components/encabezado';
 import { Menu, PedirMenu } from './components/menu';
@@ -13,10 +13,10 @@ import Pie from './components/pie';
 import Carrito from './components/carrito';
 import { IniciarSesion } from './components/iniciarsesion';
 import AdminProducto from './components/PanelAdmin';
-
-// 👇 NUEVOS IMPORTS
 import { Registrarse } from './components/registrarse';
 import { OlvidadoContrasena } from './components/olvidadocontrasena';
+import Pago from './components/pago';
+import Boleta from './components/boleta';
 
 function App() {
   return (
@@ -33,18 +33,17 @@ function App() {
 
         <Route path='nosotros' element={<Nosotros/>}/>
         <Route path='carrito' element={<Carrito/>}/>
+        <Route path='pago' element={<Pago/>}/> 
+        <Route path='boleta' element={<Boleta/>}/> 
         <Route path='iniciarsesion' element={<IniciarSesion/>}/>
         <Route path='pedirMenu' element={<PedirMenu/>}/>
-        
         <Route path='admin' element={<AdminProducto/>}/>
-
-        
         <Route path='registrarse' element={<Registrarse/>}/>
         <Route path='olvidadocontrasena' element={<OlvidadoContrasena/>}/>
       </Routes>
       <Pie/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
